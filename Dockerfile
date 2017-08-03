90166bf1dcc040fcef2ab0c2c9443e59c67768aa
Clarence Ho <clarence@Clarences-MacBook-Pro.local>
2017-08-03 15:26:21 +0800

    Minor changes

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM golang:latest
 RUN mkdir /app 
 ADD . /app/ 
 WORKDIR /app 
-RUN go build -o main . 
+RUN go get github.com/hashicorp/logutils \
+    && go build -o main . 
 CMD ["/app/main"]
--- gotest.go
+++ gotest.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"errors"
 	"log"
+	"os"
 	"time"
+
+	"github.com/hashicorp/logutils"
 )
 
 func main() {
 
+	filter := &logutils.LevelFilter{
+		Levels: []logutils.LogLevel{"DEBUG", "INFO", "WARN", "ERROR"},
+		MinLevel: logutils.LogLevel("DEBUG"),
+		Writer: os.Stderr,
+	}
+	log.SetOutput(filter)
+
 	count := 1
 
+	err := errors.New("Some application error")
+
 	for {
+		log.Printf("[DEBUG] Received GpsTransaction with key: %d", count)
+		log.Printf("[INFO] Received GpsTransaction with key: %d", count)
+		log.Printf("[WARN] Received GpsTransaction with key: %d", count)
+		log.Printf("[ERROR] Received GpsTransaction with key: %d, err: %s", count, err)
 		log.Printf("Received GpsTransaction with key: %d", count)
 		count++
-		time.Sleep(2 * time.Second)
+		time.Sleep(10 * time.Second)
 	}
 
 }
